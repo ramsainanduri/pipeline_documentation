89ac97a89ef6a8c191fa4ffabefc83c987812858
ramsainanduri <sai.rama7412@gmail.com>
2023-02-13 16:35:28 +0100

    Added multiple child Rmds and added the order of the files in configs

--- bin/runMe.sh
+++ bin/runMe.sh
@@ -4,8 +4,8 @@
 version='v1.0'
 author='Ram Sai Nanduri'
 git_repo='https://github.com/ramsainanduri/pipeline_documentation.git'
-pd_path=$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
-example_command="bash $(realpath $0) -g ${git_repo} -o ${pd_path}/../pipeline_documentation.html -y ${pd_path}/../templates/pipeline.yaml -s ${pd_path}/../envs/pipeline_documentation_v1.0.sif"
+pd_path=$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P |sed 's/\/bin$//g')
+example_command="bash $(realpath $0) -g ${git_repo} -o ${pd_path}/pipeline_documentation.html -y ${pd_path}/templates/pipeline.yaml -s ${pd_path}/envs/pipeline_documentation_v1.0.sif"
 ################################################################################
 
 ################################################################################
@@ -38,7 +38,7 @@ do
         s) singularity_container=${OPTARG};;
         y) yaml_file=${OPTARG};;
         e) #create example_command.sh
-            echo -e "${example_command}\n" > ${pd_path}/../example_command.sh
+            echo -e "${example_command}\n" > ${pd_path}/example_command.sh
             exit;;
         v) #print version
             echo -e "${version}"
@@ -57,24 +57,25 @@ done
 
 #pipeline documentation repo path and files
 date=$(date '+%Y%m%d')
-rmd_file=${pd_path}"/../templates/template.Rmd"
-template_mkdocs_yaml=${pd_path}"/../templates/template.mkdocs.yaml"
-local_project_location=$(grep 'location:' ${yaml_file} | sed 's/location:.* //g' | sed 's/ //g')
+rmd_file=${pd_path}"/bin/main.Rmd"
+template_mkdocs_yaml=${pd_path}"/templates/template.mkdocs.yaml"
+local_project_location=$(grep 'location:' ${yaml_file} | sed 's/server_location:.* //g' | sed 's/ //g')
 github_project_name=$(basename ${github_project_URL} | sed 's/.git//g')
 tmp_dir=${local_project_location}"/tmp"
 md_output=$(echo ${output_file} |sed 's/.html$/.md/g')
 md_destination=${local_project_location}'/docs/index.md'
 
 #singularity specific
-binds=${pd_path}'/../,'${local_project_location}
+binds=${pd_path}','${local_project_location}
 singularity_cmd='singularity run --bind'
 
 #main Rmd cmd
-rmd_cmd='R -e "library(yaml);params=yaml::read_yaml('\'${yaml_file}\'');params;rmarkdown::render('\'$rmd_file\'', output_file='\'${output_file}\'', intermediates_dir='\'${tmp_dir}\'', knit_root_dir='\'${tmp_dir}\'', clean=F)"'
+rmd_cmd='R -e "library(yaml);params=yaml::read_yaml('\'${yaml_file}\'');root='\'${pd_path}/\'';params;rmarkdown::render('\'$rmd_file\'', output_file='\'${output_file}\'', intermediates_dir='\'${tmp_dir}\'', knit_root_dir='\'${tmp_dir}\'', clean=F, envir = parent.frame())"'
 
 #Generate script file
 rm ${local_project_location}/run.sh
 echo -e "mkdir -p ${tmp_dir}" >> ${local_project_location}/run.sh
+echo -e "cp ${pd_path}/bin/*Rmd ${tmp_dir}" >> ${local_project_location}/run.sh
 echo -e "\n${rmd_cmd}\n" >> ${local_project_location}/run.sh
 
 #Copying the markdown file to the projects docs folder
@@ -86,13 +87,15 @@ echo -e "echo -e \"site_name: ${github_project_name}\\\nsite_url: ${github_proje
 
 echo -e "grep -v \"site_name\|site_url\" ${template_mkdocs_yaml} >> ${local_project_location}/mkdocs.yml" >> ${local_project_location}/run.sh
 
-echo -e "cp ${pd_path}/../templates/requirements.txt ${local_project_location}/docs/" >> ${local_project_location}/run.sh
-echo -e "cp ${pd_path}/../templates/readthedocs.yaml ${local_project_location}/.readthedocs.yaml" >> ${local_project_location}/run.sh
+echo -e "cp ${pd_path}/templates/requirements.txt ${local_project_location}/docs/" >> ${local_project_location}/run.sh
+echo -e "cp ${pd_path}/templates/readthedocs.yaml ${local_project_location}/.readthedocs.yaml" >> ${local_project_location}/run.sh
 
-echo -e "cp ${md_output} ${md_destination}" >> ${local_project_location}/run.sh
+#echo -e "cp ${md_output} ${md_destination}" >> ${local_project_location}/run.sh
 
-echo -e "cp ${pd_path}/../templates/github_pages.template.yaml ${local_project_location}/docs/_config.yaml" >> ${local_project_location}/run.sh
-#echo -e "mv ${local_project_location}/mkdocs.yml ${local_project_location}/docs/mkdocs.yaml" >> ${local_project_location}/run.sh
+echo -e "cp ${pd_path}/templates/github_pages.template.yaml ${local_project_location}/docs/_config.yaml" >> ${local_project_location}/run.sh
+echo -e "cp ${tmp_dir}/*.md ${local_project_location}/docs/" >> ${local_project_location}/run.sh
+echo -e "mv ${local_project_location}/docs/info.md ${local_project_location}/docs/index.md" >> ${local_project_location}/run.sh
+echo -e "rm ${local_project_location}/docs/main.knit.md" >> ${local_project_location}/run.sh
 
 echo -e "rm -rf ${tmp_dir}" >> ${local_project_location}/run.sh
 echo -e "rm ${md_output}" >> ${local_project_location}/run.sh
---
+++ docs/workflow.md
@@ -0,0 +1,7 @@
+
+# Analysis Workflow  
+
+Schematic diagram showing the main steps of the analysis method followed to perform the data analysis.    
+
+
+![]( /home/ramsainanduri/Pipelines/Validation_Documents/pipeline_documentation/templates/template_workflow.png )
